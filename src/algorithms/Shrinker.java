4ce0dfae53a3cafb3d7e9a4bdcc4ebd466936ee8
Darjus Hosszejni <hdarjus@gmail.com>
2015-11-23 16:31:38 +0100

    resolve warnings

--- src/algorithms/Shrinker.java
+++ src/algorithms/Shrinker.java
@@ -1,14 +1,8 @@
 package algorithms;
 
-import java.io.IOException;
-
 import org.opencv.core.Core;
 import org.opencv.core.CvType;
 import org.opencv.core.Mat;
-import org.opencv.core.Scalar;
-
-import imageProcessing.ShowImage;
-import imageProcessing.WriteImage;
 
 public class Shrinker {
 
